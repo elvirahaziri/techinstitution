37b41392ae55c959e028cecd3e01ebddcd9b4ee9
Elvira Haziri <elviraahaziri@gmail.com>
2017-11-23 10:39:44 +0100

    changes made

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+console.log("Hello World");
+function showName (name)
+{
+	console.log("Emri im eshte:", name);
+}
+showName("Elvira");
+
+function vendlindja(vendi,shteti)
+{
+	console.log("Vendlindja eshte :" ,vendi+",",shteti)
+}
+vendlindja("Prishtina","Kosove");
